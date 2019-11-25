18ffc2f3b51d8e32b3a1d03a1f2016284cbb50fe
mderijk <mderijk@github.com>
2019-11-25 14:01:34 +0100

    Update client to version 1.5

--- website/public/js/client.js
+++ website/public/js/client.js
@@ -3,7 +3,7 @@
 function Client() {
 	this.request_lock = false;
 	this.session = JSON.parse(localStorage.getItem('session')); // id, username, in_game
-	this.version = '1.4';
+	this.version = '1.5';
 	
 	this.sendAjaxRequest = function(url, data, callback) {
 		if (this.request_lock === false) {
